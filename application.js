6d92a0506ced9b612fca99c7092cdcd1440d7f6b
Navya <navyadevineni30@gmail.com>
2020-03-22 19:29:32 -0500

    committed

---
+++ application.js
@@ -0,0 +1,42 @@
+(function($) {
+    "use strict"; // Start of use strict
+  
+    // Smooth scrolling using jQuery easing
+    $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function() {
+      if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+        var target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+        if (target.length) {
+          $('html, body').animate({
+            scrollTop: (target.offset().top)
+          }, 1000, "easeInOutExpo");
+          return false;
+        }
+      }
+    });
+    
+    function volume_sphere()
+   {
+    var volume;
+    var radius = document.getElementById('radius').value;
+    radius = Math.abs(radius);
+    volume = (4/3) * Math.PI * Math.pow(radius, 3);
+    volume = volume.toFixed();
+    document.getElementById('volume').value = volume;
+    return false;
+   } 
+  window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
+  
+  
+    // Closes responsive menu when a scroll trigger link is clicked
+    $('.js-scroll-trigger').click(function() {
+      $('.navbar-collapse').collapse('hide');
+    });
+  
+    // Activate scrollspy to add active class to navbar items on scroll
+    $('body').scrollspy({
+      target: '#sideNav'
+    });
+  
+  })(jQuery); // End of use strict
+  
